cc4792138f3fb0229c8be361478014918562f566
Tinuriel <tinuriel@gmail.com>
2024-02-19 13:28:01 +0100

    Update and rename Fitjet to widget.js

---
+++ widget.js
@@ -0,0 +1,16 @@
+// widget.js
+
+// Function to format date and time nicely
+function formatDateTime(date) {
+    const options = { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric', minute: 'numeric', second: 'numeric' };
+    return date.toLocaleDateString('en-US', options);
+}
+
+// Function to update the widget content
+function updateWidget() {
+    const currentDate = new Date();
+    const formattedDateTime = formatDateTime(currentDate);
+    document.getElementById('widget').innerText = formattedDateTime;
+}
+
+// Update the wi
